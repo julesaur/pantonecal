16e816db46fd091bd39d04fbedb2159af23150ac
julesaur <julee.nhek@sjsu.edu>
2021-10-22 18:41:14 -0700

    include these codes

--- script.js
+++ script.js
@@ -1,66 +1,71 @@
-function jan(value){
+function jan(value) {
 
-      var element = document.getElementById("jancolor");
-        element.style.backgroundColor = value;
-    }
+  var element = document.getElementById("jancolor");
+  element.style.backgroundColor = value;
+}
 
-    function feb(value){
+function feb(value) {
 
-      var element = document.getElementById("febcolor");
-        element.style.backgroundColor = value;
-    }
-    function mar(value){
+  var element = document.getElementById("febcolor");
+  element.style.backgroundColor = value;
+}
 
-      var element = document.getElementById("marcolor");
-        element.style.backgroundColor = value;
-    }
+function mar(value) {
 
-    function apr(value){
+  var element = document.getElementById("marcolor");
+  element.style.backgroundColor = value;
+}
 
-      var element = document.getElementById("aprcolor");
-        element.style.backgroundColor = value;
-    }
-    function may(value){
+function apr(value) {
 
-      var element = document.getElementById("maycolor");
-        element.style.backgroundColor = value;
-    }
+  var element = document.getElementById("aprcolor");
+  element.style.backgroundColor = value;
+}
 
-    function jun(value){
+function may(value) {
 
-      var element = document.getElementById("juncolor");
-        element.style.backgroundColor = value;
-    }
-    function jul(value){
+  var element = document.getElementById("maycolor");
+  element.style.backgroundColor = value;
+}
 
-      var element = document.getElementById("julcolor");
-        element.style.backgroundColor = value;
-    }
+function jun(value) {
 
-    function aug(value){
+  var element = document.getElementById("juncolor");
+  element.style.backgroundColor = value;
+}
 
-      var element = document.getElementById("augcolor");
-        element.style.backgroundColor = value;
-    }
-    function sep(value){
+function jul(value) {
 
-      var element = document.getElementById("sepcolor");
-        element.style.backgroundColor = value;
-    }
+  var element = document.getElementById("julcolor");
+  element.style.backgroundColor = value;
+}
 
-    function oct(value){
+function aug(value) {
 
-      var element = document.getElementById("octcolor");
-        element.style.backgroundColor = value;
-    }
-    function nov(value){
+  var element = document.getElementById("augcolor");
+  element.style.backgroundColor = value;
+}
 
-      var element = document.getElementById("novcolor");
-        element.style.backgroundColor = value;
-    }
+function sep(value) {
 
-    function dec(value){
+  var element = document.getElementById("sepcolor");
+  element.style.backgroundColor = value;
+}
 
-      var element = document.getElementById("deccolor");
-        element.style.backgroundColor = value;
-    }
+function oct(value) {
+
+  var element = document.getElementById("octcolor");
+  element.style.backgroundColor = value;
+}
+
+function nov(value) {
+
+  var element = document.getElementById("novcolor");
+  element.style.backgroundColor = value;
+}
+
+function dec(value) {
+
+  var element = document.getElementById("deccolor");
+  element.style.backgroundColor = value;
+}
