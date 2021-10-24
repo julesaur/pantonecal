06cc24da5fcb55e33c6911cac0e0d61b2f99e05e
julesaur <julee.nhek@sjsu.edu>
2021-10-23 21:13:15 -0700

    finish color :D

--- script.js
+++ script.js
@@ -1,71 +1,54 @@
 function jan(value) {
-
   var element = document.getElementById("jancolor");
   element.style.backgroundColor = value;
 }
 
 function feb(value) {
-
   var element = document.getElementById("febcolor");
   element.style.backgroundColor = value;
 }
-
 function mar(value) {
-
   var element = document.getElementById("marcolor");
   element.style.backgroundColor = value;
 }
 
 function apr(value) {
-
   var element = document.getElementById("aprcolor");
   element.style.backgroundColor = value;
 }
-
 function may(value) {
-
   var element = document.getElementById("maycolor");
   element.style.backgroundColor = value;
 }
 
 function jun(value) {
-
   var element = document.getElementById("juncolor");
   element.style.backgroundColor = value;
 }
-
 function jul(value) {
-
   var element = document.getElementById("julcolor");
   element.style.backgroundColor = value;
 }
 
 function aug(value) {
-
   var element = document.getElementById("augcolor");
   element.style.backgroundColor = value;
 }
-
 function sep(value) {
-
   var element = document.getElementById("sepcolor");
   element.style.backgroundColor = value;
 }
 
 function oct(value) {
-
   var element = document.getElementById("octcolor");
   element.style.backgroundColor = value;
 }
-
 function nov(value) {
-
   var element = document.getElementById("novcolor");
   element.style.backgroundColor = value;
 }
 
 function dec(value) {
-
   var element = document.getElementById("deccolor");
   element.style.backgroundColor = value;
 }
